fdea5e7b0e231a26912a5d4443577615a1da3db9
Anthony Thibodeaux <actninswitch@gmail.com>
2023-02-23 08:37:29 -0700

    file structure established

---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      NAV
+    </div>
+  )
+}
+
+export default Nav;
+
---
+++ src/Components/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      REGISTER
+    </div>
+  )
+}
+
+export default Register;
