1c7a437fd8c7be3950621dde1cc636056dc52428
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-02-23 12:08:47 -0500

    added test folder and files associated with our tables

---
+++ tests/db/user.spec.js
@@ -0,0 +1,14 @@
+//make sure to make a dummy database to test out the data and functions on
+//need to make sure we don't save it in the main branch that way, but add it to the local client
+function sum(a, b) {
+    {
+    return a + b;
+  }
+  }
+
+  describe('testing sum function', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+      });
+  })
+  
