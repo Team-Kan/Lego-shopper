99021f483d63cd82db5d550babd973f2d1ff2565
Anthony Thibodeaux <actninswitch@gmail.com>
2023-02-24 17:52:32 -0700

    got editIsAdmin done and removed password from users we return

--- server/db/User.js
+++ server/db/User.js
@@ -1,54 +1,73 @@
-const client = require('./client');
-const jwt = require('jsonwebtoken');
+const client = require("./client");
+const jwt = require("jsonwebtoken");
 const JWT = process.env.JWT;
 
-
-const createUser = async({ username, password }) => {
+const createUser = async ({ username, password }) => {
+  // we are now not returning the password
   const SQL = `
     INSERT INTO users(username, password)
-    VALUES($1, $2) RETURNING *
+    VALUES($1, $2) 
+    RETURNING id, username, "isAdmin"
   `;
-  const response = await client.query(SQL, [ username, password ]);
+  const response = await client.query(SQL, [username, password]);
   return response.rows[0];
-}
+};
 
-const getUserByToken = async(token) => {
+const getUserByToken = async (token) => {
   const payload = await jwt.verify(token, JWT);
   const SQL = `
-    SELECT users.*
+    SELECT id, username, "isAdmin"
     FROM users
     WHERE id = $1 
   `;
-  const response = await client.query(SQL, [ payload.id]);
-  if(!response.rows.length){
-    const error = Error('not authorized');
+  const response = await client.query(SQL, [payload.id]);
+  if (!response.rows.length) {
+    const error = Error("not authorized");
     error.status = 401;
     throw error;
   }
   const user = response.rows[0];
   delete user.password;
-  return user; 
-}
+  return user;
+};
 
-const authenticate = async({ username, password }) => {
+const authenticate = async ({ username, password }) => {
   const SQL = `
     SELECT id
     FROM users
     WHERE username = $1 and password = $2
   `;
-  const response = await client.query(SQL, [ username, password]);
+  const response = await client.query(SQL, [username, password]);
   console.log(response);
-  if(!response.rows.length){
-    const error = Error('not authorized');
+  if (!response.rows.length) {
+    const error = Error("not authorized");
     error.status = 401;
     throw error;
   }
   return jwt.sign({ id: response.rows[0].id }, JWT);
-}
+};
+
+const editIsAdmin = async ({ id, isAdmin }) => {
+  const {
+    rows: [user],
+  } = await client.query(
+    `
+  UPDATE users
+  SET "isAdmin" = $1
+  WHERE id = $2
+  RETURNING username, id, "isAdmin"
+  `,
+    [!isAdmin, id]
+  ); /*Turn isAdmin to the opposite,
+   so we can use the function for adding and removing admins
+   */
+
+  return user;
+};
 
 module.exports = {
   createUser,
   authenticate,
-  getUserByToken
+  getUserByToken,
+  editIsAdmin,
 };
-
--- tests/db/user.spec.js
+++ tests/db/user.spec.js
@@ -1,13 +1,44 @@
 //make sure to make a dummy database to test out the data and functions on
 //need to make sure we don't save it in the main branch that way, but add it to the local client
-function sum(a, b) {
-  {
-    return a + b;
-  }
-}
+const { createUser } = require("../../server/db");
+const { editIsAdmin } = require("../../server/db/User");
+const setup = require("../setup");
+const tearDown = require("../tearDown");
 
-describe("testing sum function", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+beforeAll(async () => {
+  await setup();
+});
+
+afterAll(async () => {
+  await tearDown();
+});
+
+describe("createUser()", () => {
+  it("doesn't return the password", async () => {
+    const user = await createUser({ username: "dan", password: "isPassword" });
+
+    expect(user.password).toBe(undefined);
+  });
+  it("to have isAdmin false by default", async () => {
+    const secondUser = await createUser({
+      username: "danny",
+      password: "isPassword2",
+    });
+
+    expect(secondUser.isAdmin).toBe(false);
+  });
+});
+
+describe("editIsAdmin() editing isAdmin, with true or false.", () => {
+  it("to turn isAdmin to be true if it is false and it to be false if true", async () => {
+    const thirdUser = await createUser({
+      username: "notDan",
+      password: "thisispassword",
+    });
+
+    const newAdmin = await editIsAdmin(thirdUser);
+    expect(newAdmin.isAdmin).toBe(true);
+    const goodbyeAdmin = await editIsAdmin(newAdmin);
+    expect(goodbyeAdmin.isAdmin).toBe(false);
   });
 });
