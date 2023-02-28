76d06e75926590701e065fa7590f9fb203307474
nbeelahamadeen <nbeelahamadeen@gmail.com>
2023-02-28 14:56:16 -0600

    added router.get call for getAllProducts

--- server/api/product.js
+++ server/api/product.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const {getAllProducts} = require("../db/product")
+const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+    try {
+        const products = await getAllProducts();
+
+        res.send(products);
+    } catch (error) {
+        next(error);
+    }
+})
