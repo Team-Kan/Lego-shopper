a1f54dde211a4afee3936e5fa3807c4d5cf2da91
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-03-01 12:13:09 -0500

    created links for individual collections

--- src/Components/Collections.js
+++ src/Components/Collections.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Collections = () => {
-  return (
-    <div>Collections</div>
-  )
+const Collections = (props) => {
+    const { collections } = props;
+    return (
+        <>
+        <h1>Collections</h1>
+        {
+            collections.map(collection => {
+                return (
+                    <>
+                        <Link to={`/collections/${collection.id}`}>{collection.name}</Link>
+                    </>
+                )
+            })
+        }
+        </>
+    )
 }
 
 export default Collections;
