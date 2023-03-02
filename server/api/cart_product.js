6e049dd567906d086bd51ab705941fc93f512c94
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-03-02 14:26:29 -0500

    added api code for cart_products

--- server/api/cart_product.js
+++ server/api/cart_product.js
@@ -1,16 +1,37 @@
 const express = require("express");
+const { addProductToCart, editCartProduct, deleteCartProduct } = require("../db/cart_product");
+const { tokenAuth } = require("./utils");
 const router = express.Router();
+
 // TODO
-router.post("/", async (req, res, next) => {
-  
+router.post("/", tokenAuth, async (req, res, next) => {
+  try {
+    const { cartId, productId, quantity } = req.body;
+    const cart_product = await addProductToCart({ cartId , productId, quantity });
+    res.send(cart_product);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.patch("/", async (req, res, next) => {
-  
+  try {
+    const { cartId, productId, quantity } = req.body;
+    const cart_product = await editCartProduct(cartId, productId, quantity);
+    res.send(cart_product);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.delete("/", async (req, res, next) => {
-  
+    try {
+        const { cartId, productId } = req.body;
+        const cart_product = await deleteCartProduct(cartId, productId);
+        res.send(cart_product);
+      } catch (error) {
+        next(error);
+      }
 })
 
 module.exports = router;
