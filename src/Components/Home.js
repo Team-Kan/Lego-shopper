852e020ae7ea1750cb20a7c10c06096bfa90570e
nbeelahamadeen <nbeelahamadeen@gmail.com>
2023-03-04 16:12:59 -0600

    minor css changes to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,8 +6,8 @@ const Home = (props)=> {
   const {products, collections} = props;
   return (
     <div>
-      <h1>Home</h1>
       <Collections collections={collections} />
+      <img className='welcome_image' src='https://images.unsplash.com/photo-1585366119957-e9730b6d0f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'/>
       <Products products={products}/>
     </div>
   );
