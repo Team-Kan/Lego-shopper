6f25a5eb4528bb75c131cb76b72cbf7991f73476
Anthony Thibodeaux <actninswitch@gmail.com>
2023-03-04 12:17:45 -0700

    got singleProduct page started

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 const SingleProduct = () => {
+  
+  const [product, setProduct] = useState("");
+  const location = useLocation();
+  const pathName = location.pathname;
+  const pathArray = pathName.split("/");
+  const id = pathArray[pathArray.length - 1];
+
+  const getProduct = async () => {
+    
+  }
+
+  useEffect(() => {
+    getProduct()
+  }, [id]);
+
   return (
     <div>SingleProduct</div>
   )
