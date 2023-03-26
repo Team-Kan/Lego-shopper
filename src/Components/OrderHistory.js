05c154f4530e6081a51438703849d74ae3739ead
Tildozer <actninswitch@gmail.com>
2023-03-26 15:04:40 -0600

    got orderHistory started, just need to figure out how to get total for previous orders

--- src/Components/OrderHistory.js
+++ src/Components/OrderHistory.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const OrderHistory = (props) => {
-    const {orderHistory} = props;
-   
+  const { orderHistory } = props;
+
   return (
-    <div>
-      {orderHistory.length 
-        ? ( 
-          orderHistory.map(order => {
+    <div className="flex flex-col items-center">
+      {orderHistory.length ? (
+        <ul className="w-8/12">
+          {orderHistory.map((order) => {
             return (
-              <div key={order.id}>
-                {order.id}
-              </div>
-            )
-          }))
-        : (
-          <div className='bg-yellow-500 text-4xl'>
-            You have no previous orders... yet!
-          </div>
-        )
-      }
+              <li key={order.id} className="bg-slate-100 p-4">
+                <ul>
+                  {order.products.map((product) => {
+                    return (
+                      <li key={product.id}>
+                        <div>
+                          <span>{product.name} </span>
+                          <div className="flex">
+                            <img
+                              className="h-16 w-16 border-2 border-green-600 mr-2"
+                              src={product.imageUrl}
+                            />
+                            <span>amount purchased: {product.quantity}</span>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="bg-yellow-500 text-4xl">
+          You have no previous orders... yet!
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default OrderHistory;
